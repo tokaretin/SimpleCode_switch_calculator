d417683835fe76e2c2e4db157fa294c37522f406
tokaretin <tokaretin@gmail.com>
2023-05-12 08:35:15 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode_switch_calculator/SimpleCode_switch_calculator.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, selection;
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+        
+    cout << "Нажмите 1: Умножение:\n"
+        << "Нажмите 2: Деление:\n"
+        << "Нажмите 3: Разность:\n"
+        << "Нажмите 4: Сложение:\n";
+    cin >> selection;
+
+    switch (int(selection))
+    {
+    case 1:
+        cout << a << " * " << b << " = " << a * b;
+        break;
+    case 2:
+        cout << a << " / " << b << " = " << a / b;
+        break;
+    case 3:
+        cout << a << " - " << b << " = " << a - b;
+        break;
+    case 4:
+        cout << a << " + " << b << " = " << a + b;
+        break;
+    default:
+        cout << "Пункт меню отсутстыует";
+        break;
+    }
+
+}
+
